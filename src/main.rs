4e2872662db3dd990bdbe05aac408792b6ee8849
Rémy Raes <contact@remyraes.com>
2023-07-03 11:13:54 +0200

    feat: add a cron to save scores list to disk periodically

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use parking_lot::RwLock;
 use std::{sync::Arc, fs::File};
 use serde::{Serialize, Deserialize};
 use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
 
 type ScoreEntries = Vec<ScoreEntry>;
 
@@ -69,6 +71,38 @@ fn post_json() -> impl Filter<Extract = (ScoreEntry,), Error = warp::Rejection> 
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
+fn list_dump_cron(store: Store) {
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_secs(5));
+            let scores = store.scores_list.read().to_vec();
+
+            let mut buffer = match File::create("list.json") {
+                Ok(file) => file,
+                Err(err) => {
+                    println!("Error: \".list.json\" file could not be created [{}].", err);
+                    std::process::exit(3);
+                }
+            };
+
+            let str = match serde_json::to_string(&scores) {
+                Ok(str) => str,
+                Err(err) => {
+                    println!("Error: failed serializing scores list [{}].", err);
+                    std::process::exit(3);
+                }
+            };
+            match buffer.write(str.as_bytes()) {
+                Ok(str) => str,
+                Err(err) => {
+                    println!("Error: failed writing scores list to file [{}].", err);
+                    std::process::exit(3);
+                }
+            };
+        }
+    });
+}
+
 #[tokio::main]
 async fn main() {
     // Secret key
@@ -93,6 +127,8 @@ async fn main() {
     let accept_requests = warp::header::exact("authentication", header_value);
 
     let store = Store::new();
+    // Scores saving cron
+    list_dump_cron(store.clone());
     let store_filter = warp::any().map(move || store.clone());
 
 
