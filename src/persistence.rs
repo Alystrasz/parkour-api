b45059d2d9d2c193ffbedf6a79abe0677710f418
Remy Raes <contact@remyraes.com>
2023-07-17 22:01:09 +0200

    feat: load/save events from/to JSON file

--- src/persistence.rs
+++ src/persistence.rs
@@ -2,9 +2,11 @@ use std::env;
 use std::{thread, time::Duration, fs::File};
 use std::io::prelude::*;
 
+use crate::event::Events;
 use crate::map::Maps;
 use crate::{Store, ScoreEntries, log};
 
+const EVENTS_FILE: &str = "events.json";
 const MAPS_FILE: &str = "maps.json";
 const SCORES_FILE: &str = "scores.json";
 
@@ -73,6 +75,31 @@ pub fn start_save_cron(store: Store) {
                 }
             };
             log::info("Saved maps to local file.");
+
+            // Events
+            let events = store.events_list.read().clone();
+            let mut buffer = match File::create(EVENTS_FILE) {
+                Ok(file) => file,
+                Err(err) => {
+                    log::error(&format!("\"{}\" file could not be created [{}].", EVENTS_FILE, err));
+                    std::process::exit(3);
+                }
+            };
+            let str = match serde_json::to_string(&events) {
+                Ok(str) => str,
+                Err(err) => {
+                    log::error(&format!("Failed serializing events list [{}].", err));
+                    std::process::exit(3);
+                }
+            };
+            match buffer.write(str.as_bytes()) {
+                Ok(str) => str,
+                Err(err) => {
+                    log::error(&format!("Failed writing events list to file [{}].", err));
+                    std::process::exit(3);
+                }
+            };
+            log::info("Saved events to local file.");
         }
     });
 }
@@ -135,4 +162,33 @@ pub fn load_state(store: Store) {
         write_lock.insert(key, value);
     }
     log::info(&format!("Loaded maps list from \"{}\" file.", MAPS_FILE));
+
+    // Events
+    let mut file = match File::open(EVENTS_FILE) {
+        Ok(file) => file,
+        Err(_) => {
+            log::info(&format!("\"{}\" file does not exist, initializing events list as empty.", EVENTS_FILE));
+            return;
+        }
+    };
+    let mut data = String::new();
+    match file.read_to_string(&mut data) {
+        Ok(_) => (),
+        Err(err) => {
+            log::error(&format!("Failed reading \"{}\" file [{}].", EVENTS_FILE, err));
+            std::process::exit(2);
+        }
+    };
+    let serialized: Events = match serde_json::from_str::<Events>(&data) {
+        Ok(data) => data,
+        Err(err) => {
+            log::error(&format!("Failed deserializing events list [{}].", err));
+            std::process::exit(2);
+        }
+    };
+    let mut write_lock = store.events_list.write();
+    for value in serialized {
+        write_lock.push(value);
+    }
+    log::info(&format!("Loaded events list from \"{}\" file.", EVENTS_FILE));
 }
