b5dd757bdc99a13be252b1cf5131ab0d7f0ed758
Remy Raes <contact@remyraes.com>
2023-08-09 23:22:53 +0200

    feat: display one scores table per map

--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -5,7 +5,7 @@ use serde::Serialize;
 use serde_json::json;
 use warp::{Filter, Reply, Rejection};
 
-use crate::{Store, event::Event, log};
+use crate::{Store, event::Event, log, scores::ScoreEntries};
 
 const TEMPLATE_FILE: &str = "scoreboard/template.html";
 
@@ -76,6 +76,15 @@ pub fn get_routes(store: Store) -> impl Filter<Extract = impl Reply, Error = Rej
         std::process::exit(1);
     }
     let corresponding_maps = maps.get(&event_id).unwrap().clone();
+    let maps_clone = corresponding_maps.clone();
+    let map_names: Vec<&String> = maps_clone.iter().map(|m| m.id.as_ref().unwrap()).collect();
+
+    let scores: ScoreEntries = store
+        .clone()
+        .scores_list.read()
+        .clone()
+        .into_iter()
+        .filter(|e| map_names.contains(&&e.0)).collect();
 
     let get_scoreboard_route = warp::get()
         .and(warp::path::end())
@@ -84,7 +93,7 @@ pub fn get_routes(store: Store) -> impl Filter<Extract = impl Reply, Error = Rej
             value: json!({
                 "event": corresponding_events.clone().first(),
                 "maps": corresponding_maps,
-                "scores": store.clone().scores_list.read().get("b75bd077-7198-4c5a-ba32-33e16202f320").unwrap().to_vec()
+                "scores": scores
             }),
         })
         .map(handlebars);
