393f8c3583c91a672e6ed208b4d2fe886c348823
Remy Raes <raes.remy@gmail.com>
2023-08-11 23:55:43 +0200

    fix: recompute HTML page on scoreboard route hit

--- src/scoreboard.rs
+++ src/scoreboard.rs
@@ -16,15 +16,52 @@ struct WithTemplate<T: Serialize> {
 }
 
 
-fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars<'_>>) -> impl warp::Reply
-where
-    T: Serialize,
+fn render(hbs: Arc<Handlebars<'_>>, store: Store) -> impl warp::Reply
 {
+    // Find current event
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    let events = store.clone().events_list.read().clone().into_iter();
+    let corresponding_events: Vec<Event> = events.filter(|e| now >= e.start.try_into().unwrap() && now <= e.end.try_into().unwrap()).collect();
+    if corresponding_events.len() != 1 {
+        log::error(&format!("Expected one corresponding event, found {}.", corresponding_events.len()));
+        std::process::exit(42);
+    }
+
+    // Find associated maps
+    let event = corresponding_events.first().unwrap().clone();
+    let event_id = event.id.unwrap();
+    let maps = store.clone().maps_list.read().clone();
+    if !maps.contains_key(&event_id) {
+        log::error("Current event features no map.");
+        std::process::exit(1);
+    }
+    let corresponding_maps = maps.get(&event_id).unwrap().clone();
+    let maps_clone = corresponding_maps.clone();
+    let map_names: Vec<&String> = maps_clone.iter().map(|m| m.id.as_ref().unwrap()).collect();
+
+    let scores: ScoreEntries = store
+        .clone()
+        .scores_list.read()
+        .clone()
+        .into_iter()
+        .filter(|e| map_names.contains(&&e.0)).collect();
+
+    let template = WithTemplate {
+        name: "template.html",
+        value: json!({
+            "event": corresponding_events.clone().first(),
+            "maps": corresponding_maps,
+            "scores": scores
+        }),
+    };
+
     let render = hbs
         .render(template.name, &template.value)
         .unwrap_or_else(|err| err.to_string());
+    
     warp::reply::html(render)
 }
+
 
 pub fn get_routes(store: Store) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
     // Load HTML template
@@ -71,49 +108,13 @@ pub fn get_routes(store: Store) -> impl Filter<Extract = impl Reply, Error = Rej
     let hb = Arc::new(hb);
 
     // Create a reusable closure to render template
-    let handlebars = move |with_template| render(with_template, hb.clone());
+    let handlebars = move || render(hb.clone(), store.clone());
 
     // Static route to serve CSS and JS assets
     let static_assets = warp::path("assets").and(warp::fs::dir("scoreboard/assets"));
 
-    // Find current event
-    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-    let events = store.clone().events_list.read().clone().into_iter();
-    let corresponding_events: Vec<Event> = events.filter(|e| now >= e.start.try_into().unwrap() && now <= e.end.try_into().unwrap()).collect();
-    if corresponding_events.len() != 1 {
-        log::error(&format!("Expected one corresponding event, found {}.", corresponding_events.len()));
-        std::process::exit(42);
-    }
-
-    // Find associated maps
-    let event = corresponding_events.first().unwrap().clone();
-    let event_id = event.id.unwrap();
-    let maps = store.clone().maps_list.read().clone();
-    if !maps.contains_key(&event_id) {
-        log::error("Current event features no map.");
-        std::process::exit(1);
-    }
-    let corresponding_maps = maps.get(&event_id).unwrap().clone();
-    let maps_clone = corresponding_maps.clone();
-    let map_names: Vec<&String> = maps_clone.iter().map(|m| m.id.as_ref().unwrap()).collect();
-
-    let scores: ScoreEntries = store
-        .clone()
-        .scores_list.read()
-        .clone()
-        .into_iter()
-        .filter(|e| map_names.contains(&&e.0)).collect();
-
     let get_scoreboard_route = warp::get()
         .and(warp::path::end())
-        .map(move || WithTemplate {
-            name: "template.html",
-            value: json!({
-                "event": corresponding_events.clone().first(),
-                "maps": corresponding_maps,
-                "scores": scores
-            }),
-        })
         .map(handlebars);
 
     static_assets.or(get_scoreboard_route)
